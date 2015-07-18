f639cdde43d03cf8b6ba0aeba9be5d79c6d51c6f
Miguel Victor <migzwebhost@gmail.com>
2015-07-18 16:02:14 +0800

    Added a working test to meals.groupMeals() method

--- war/karma.conf.js
+++ war/karma.conf.js
@@ -17,6 +17,11 @@ module.exports = function(config) {
     files: [
       'node_modules/angular/angular.min.js',
       'node_modules/angular-mocks/angular-mocks.js',
+      'node_modules/angular-route/angular-route.min.js',
+      'node_modules/angular-animate/angular-animate.min.js',
+      'node_modules/angular-aria/angular-aria.min.js',
+      'node_modules/angular-messages/angular-messages.min.js',
+      'node_modules/angular-material/angular-material.min.js',
       'app/*.js',
       'test/*.js'
     ],
--- war/test/unit_test.js
+++ war/test/unit_test.js
@@ -1,21 +1,17 @@
 describe('Group meals', function () {
-    var mealService;
-    
+    var mealService = null;
+
     beforeEach(module('MealJournalApp'));
-    
-    beforeEach(inject(function (meals) {
-        
+
+    beforeEach(angular.mock.inject(function (meals) {
+        mealService = meals;
     }));
-    
-    it('should be true', function () {
-        expect(true).toBeTruthy();
+
+    it('expects meals service to be defined', function () {
+        expect(mealService).toBeTruthy();
     });
-    
-    /*it('expects mealService to be defined', function () {
-        expect(mealService).toBeDefined();
-    });
-    
-    /*it('should group meals by date', function () {
+
+    it('should group meals by date', function () {
         var meals = [
             {
                 "name": "Rice",
@@ -26,9 +22,25 @@ describe('Group meals', function () {
                 "quantity": 1
             }
         ];
+
+        var date = new Date(1436154582626);
+        var temp = new Date(date.getFullYear(), date.getMonth(), date.getDate()).getTime().toString();
         
-        var mealsArray = mealService.groupMeals(meals);
-        
-        expect(mealsArray).toEqual(mealsArray);
-    });*/
+        var expectedGroupedMeals = {};
+        expectedGroupedMeals[temp] = [
+            {
+                "name": "Rice",
+                "unit": "cup",
+                "calories": 205,
+                "iconUrl": "meals-icons/rice.jpg",
+                "dateCreated": 1436154582626,
+                "quantity": 1
+            }
+        ];
+        expectedGroupedMeals[temp].totalCalories = 205;
+
+        var groupedMeals = mealService.groupMeals(meals);
+
+        expect(groupedMeals).toEqual(expectedGroupedMeals);
+    });
 });
